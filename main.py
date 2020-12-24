98d60a7613249bef32bc2db883b05f5868a929b9
kan25d5 <43536347+kan25d5@users.noreply.github.com>
2020-12-24 20:17:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from situation.predict_situation import PredictSituation
+from situation.predict_dialog_act_type import PredictDialogActType
 
 
-ps = PredictSituation("situation_predicter", is_base_situation=True)
-print(ps.predict_situation("今日の夜飯行かね？"))
+pa = PredictDialogActType("situation_predicter", is_base_situation=True)
+print(pa.predict_da("今日の夜飯行かね？"))
