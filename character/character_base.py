a06c636c54bbf508f8f23e6f95499a28a680c79d
kan25d5 <43536347+kan25d5@users.noreply.github.com>
2020-12-30 03:52:33 +0900

    フレームが更新される度にパラメータ値を評価する

--- character/character_base.py
+++ character/character_base.py
@@ -2,12 +2,14 @@ import oseti
 import pandas as pd
 from helper.cabocha_helper import USER_DIC
 from helper.helper_functions import load_json
+from numpy import sqrt
 
 
 MAX_DISTANCE = 40000
+MAX_DISTANCE = 40000
 CHARACTER_PARAM_SET = "data/character_parameter_setting.json"
-STATION_FILE_PATH = "data/stations-simplify.csv"
 UTT_PATH = "data/character_utt.json"
+STATION_FILE_PATH = "data/stations-simplify.csv"
 
 
 class CharacterBase(object):
@@ -32,6 +34,8 @@ class CharacterBase(object):
 
         # 基底クラスが保持するプライベートフィールド変数
         self.__oseti = oseti.Analyzer(USER_DIC)
+        self.__sation_df = pd.read_csv(STATION_FILE_PATH)
+        self.__pre_frame = {}
 
     def set_situation(self, situation):
         self.situation = situation
@@ -46,17 +50,27 @@ class CharacterBase(object):
     def update_point(self, text):
         sys_da = self.situation.sys_da
         user_da = self.situation.user_da
+        frame = self.situation.frame
+
         if user_da == "chatting":
             # フレームが更新されないならネガポジ値を算出
             self.__update_emotion_by_text(text)
-        elif self.situation._is_fill_frame():
-            # コンセプト毎に感情度・関心度を評価する
-            self.situation.update_parameter_by_frame()
-            for key in self.params_set.keys():
-                concept = self.situation.frame[key]
-                param_set = self.params_set[key][concept]
-                self.emotion += param_set["emotion"]
-                self.interest += param_set["interest"]
+        else:
+            for key in frame.keys():
+                if key not in self.__pre_frame.keys():
+                    if frame[key] != "":
+                        self.__update_param_by_frame(key, frame[key])
+                        self.__pre_frame.update({key: frame[key]})
+
+    def __update_param_by_frame(self, key, concept):
+        if key == "place":
+            self.__update_interest_by_distance(concept)
+            return
+        if concept not in self.params_set[key].keys():
+            return
+        param = self.params_set[key][concept]
+        self.emotion += param["emotion"]
+        self.interest += param["interest"]
 
     def __update_emotion_by_text(self, text):
         """フレームが更新されないテキストからネガポジ値を算出"""
@@ -65,3 +79,23 @@ class CharacterBase(object):
             emotion += ((2 * point - 1) / 2) * 2
 
         self.emotion += emotion
+
+    def __get_distance(self, dest):
+        """神奈川工科大学から場所までの距離を算出"""
+        # ox, oy = df[df.station == orig].iloc[0, :].values[1:3]
+        df = self.__sation_df
+        ox, oy = -44662, -56845
+        dx, dy = df[df.station == dest].iloc[0, :].values[1:3]
+        return int(round(sqrt((ox - dx) ** 2 + (oy - dy) ** 2)))
+
+    def __update_interest_by_distance(self, place):
+        """近いとの距離から感情度を加算"""
+        # ２駅間の距離を算出
+        distance = self.__get_distance(place)
+        # 距離をMAX_DISTANCE以上にしない
+        distance = distance if distance < MAX_DISTANCE else MAX_DISTANCE
+        # [0,1]で正規化
+        interest = distance / MAX_DISTANCE
+        print("interest", interest)
+
+        self.interest -= interest
--- situation/situation_eat.py
+++ situation/situation_eat.py
@@ -1,9 +1,4 @@
-import pandas as pd
 from situation.situation_base import SituationBase
-from numpy import sqrt
-
-MAX_DISTANCE = 40000
-STATION_FILE_PATH = "data/stations-simplify.csv"
 
 
 class SituationEat(SituationBase):
@@ -12,7 +7,6 @@ class SituationEat(SituationBase):
     """
 
     def __init__(self) -> None:
-        self.__sation_df = pd.read_csv(STATION_FILE_PATH)
         super().__init__(self.__str__())
 
     def update_sys_da(self, text):
@@ -23,33 +17,33 @@ class SituationEat(SituationBase):
         self.character.update_point(text)
 
         # フレーム情報が満タンな状態 → 行動する／しないを答える状態
-        if self._is_fill_frame():
-            # 総合点が閾値を超えた → 行動する
-            if self.__is_character_act():
-                if not self._is_update_frame():
-                    # FIXME: act状態でchattingされた場合は、またactする
-                    if self.sys_da != "act":
-                        # 断ったが、chattingで感情値が更新されactになったら
-                        # re-actで「やっぱ行くよ」みたいなこと言う
-                        self.sys_da = "re-act"
-                else:
-                    self.sys_da = "act"
-            # 総合点が閾値を下回った → 行動しない
+        # 総合点が閾値を超えた → 行動する
+        if self.__is_character_act():
+            if not self._is_update_frame():
+                # FIXME: act状態でchattingされた場合は、またactする
+                if self.sys_da != "act":
+                    # 断ったが、chattingで感情値が更新されactになったら
+                    # re-actで「やっぱ行くよ」みたいなこと言う
+                    self.sys_da = "re-act"
             else:
+                self.sys_da = "act"
+        # 総合点が閾値を下回った → 行動しない
+        else:
+            if self._is_fill_frame():
                 if not self._is_update_frame():
                     # chattingで感情値が更新されたが、
                     # 行動には至らなかったら「でもごめんね」みたいなこと言う
                     self.sys_da = "re-not-act"
                 else:
                     self.sys_da = "not-act"
-        else:
-            # 足らない属性があれば、それを尋ねる
-            if self.frame["date"] == "":
-                self.sys_da = "ask-date"
-            elif self.frame["genre"] == "":
-                self.sys_da = "ask-genre"
-            elif self.frame["place"] == "":
-                self.sys_da = "ask-place"
+            else:
+                # 足らない属性があれば、それを尋ねる
+                if self.frame["date"] == "":
+                    self.sys_da = "ask-date"
+                elif self.frame["genre"] == "":
+                    self.sys_da = "ask-genre"
+                elif self.frame["place"] == "":
+                    self.sys_da = "ask-place"
 
         return self.sys_da
 
@@ -63,30 +57,10 @@ class SituationEat(SituationBase):
 
     def update_parameter_by_frame(self):
         """フレーム情報から感情度を算出"""
-        if self.user_da == "anser-place":
-            self.__update_emotion_by_distance(self.frame["place"])
+        pass
 
     def __is_character_act(self):
         return self.character.threshold_point <= self.character.calculate_point()
-
-    def __get_distance(self, dest):
-        """神奈川工科大学から場所までの距離を算出"""
-        ox, oy = -44662, -56845
-        dx, dy = (
-            self.__sation_df[self.__sation_df.station == dest].iloc[0, :].values[1:3]
-        )
-        return int(round(sqrt((ox - dx) ** 2 + (oy - dy) ** 2)))
-
-    def __update_emotion_by_distance(self, place):
-        """近いとの距離から感情度を加算"""
-        # ２駅間の距離を算出
-        distance = self.__get_distance(place)
-        # 距離をMAX_DISTANCE以上にしない
-        distance = distance if distance < MAX_DISTANCE else MAX_DISTANCE
-        # [0,1]で正規化
-        emotion = distance / MAX_DISTANCE
-
-        self.character.emotion -= emotion
 
     def __str__(self) -> str:
         return "situation-eat"
